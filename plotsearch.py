6d156835720c6a2cc1753c1d041c4fe6410d2476
Brian Carcich <briantcarcich@gmail.com>
2024-11-18 16:22:33 -0500

    Add script to plot data from SOPDT-GD-py output

---
+++ plotsearch.py
@@ -0,0 +1,19 @@
+import sys
+import matplotlib.pyplot as plt
+searchlog = (['searchE.log']
+             + [s for s in sys.argv[1]
+                if s.startswith('search') and s.endswith('.log')]
+            ).pop()
+indices = dict(zip('fxtol gain t0 t1 off dt'.split(),range(6)))
+name = (['dt']+[s for s in sys.argv[1:] if s in indices]).pop()
+index = indices[name] + 5
+
+with open(searchlog,'r') as f:
+  a=[float(L.split()[index])
+     for L in f
+     if L.startswith('MSE = ') and ' fxtol = ' in L
+    ]
+plt.plot(a)
+plt.title(name)
+if 'fxtol' == name: plt.semilogy()
+plt.show()
